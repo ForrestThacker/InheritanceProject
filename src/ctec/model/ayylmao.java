a0aa46e4855f1e3e832abb41fe7d6e6dc68af92a
ForrestThacker <fyruskasai@gmail.com>
2016-03-30 13:46:26 -0600

    Added ayylmao

---
+++ src/ctec/model/ayylmao.java
@@ -0,0 +1,16 @@
+package ctec.model;
+
+public class ayylmao 
+
+	package ayylmao.model;
+
+	public interface AyyLmaoInterface
+	{
+	public boolean tokes();
+	public double sizeOfHead();
+	public boolean HarmlessAyyLmao();
+
+	}
+
+
+}
